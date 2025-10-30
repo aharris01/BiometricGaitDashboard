2ce34fed321a00cd43cb7fe7120190875e6df92e
aharris01 <aidanharrison11@gmail.com>
2025-10-30 12:05:06 -0300

    DB init scripts use the correct search path

--- docker/db/initdb/02_create_tables.sql
+++ docker/db/initdb/02_create_tables.sql
@@ -1,10 +1,13 @@
+SET search_path TO app,
+    public;
 CREATE TABLE swipe_event (
     event_id TEXT PRIMARY KEY,
+    -- Human readable format ie. ('001_yyyy-mm-dd_(in|out)_event#_state')
     participant INT NOT NULL,
     date DATE NOT NULL,
-    direction TEXT NOT NULL,
+    direction TEXT NOT NULL CHECK (direction IN ('in', 'out')),
     event_number INT NOT NULL,
-    state TEXT NOT NULL,
+    state TEXT NOT NULL CHECK (state IN ('building', 'ready', 'failed')),
     -- 'ready' can be read, 'building' being used by another process, 'failed' corrupted or unusable
     trial_npz_uri TEXT NOT NULL,
     -- URI for the file destination
--- docker/db/initdb/03_create_indexes.sql
+++ docker/db/initdb/03_create_indexes.sql
@@ -1,3 +1,5 @@
+SET search_path to app,
+    public;
 -- Used for fast lookup of a swipe event
 CREATE INDEX swipe_event_lookup_idx ON swipe_event (participant, date, direction, event_number)
 WHERE state = 'ready';
