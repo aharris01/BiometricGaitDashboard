98b6e5bae04f59695c87441d1b993f0fc69230f9
aharris01 <aidanharrison11@gmail.com>
2026-01-19 11:11:08 -0400

    Created schema for local events

--- backend/storage_access_layer/db/schema.py
+++ backend/storage_access_layer/db/schema.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Text, Date, Integer, TIMESTAMP
+from sqlalchemy import String, Text, Date, Integer, TIMESTAMP, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 import datetime
 
@@ -16,3 +16,18 @@ class SwipeEvent(ManifestBase):
     direction: Mapped[str] = mapped_column(String, nullable=False)
     event_number: Mapped[int] = mapped_column(Integer, nullable=False)
     local: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+
+
+class LocalBase(DeclarativeBase):
+    pass
+
+
+class LocalSwipeEvent(LocalBase):
+    __tablename__ = "local_swipe_event"
+
+    event_id: Mapped[str] = mapped_column(String, primary_key=True)
+    root_path: Mapped[str] = mapped_column(String, nullable=False)
+
+    # These columns are to determine if there are any changes to the available data
+    present: Mapped[int] = mapped_column(Boolean, nullable=False)
+    last_seen: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, nullable=False)
