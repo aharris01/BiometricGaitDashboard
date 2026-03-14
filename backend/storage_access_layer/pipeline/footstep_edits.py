2cdf02e902c35ea374709efe6b5950cc21353465
Aidan Harrison <aidanharrison11@gmail.com>
2026-03-14 12:12:05 -0300

    Moved functions to pipeline_utils and perform path tracing

--- backend/storage_access_layer/pipeline/footstep_edits.py
+++ backend/storage_access_layer/pipeline/footstep_edits.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 from ..db.db import DB
 from ..helpers.common import CommonHelper
-from .utils.pipeline_utils import load_metadata, identify_anchor_footstep
+from .utils.pipeline_utils import (
+    get_heading,
+    load_metadata,
+    identify_anchor_footstep,
+    _is_within_expect_duration,
+    _is_within_expected_bb_size,
+    reset_path_order,
+    trace_path,
+)
 from flask import current_app
-
-T_MIN = 50
-T_MAX = 400
-
-SHORT_DIMENSTION_MIN = 15
-SHORT_DIMENSTION_MAX = 80
-LONG_DIMENSION_MIN = 40
-LONG_DIMENSION_MAX = 100
 
 
 class FootstepEditor:
@@ -33,8 +33,18 @@ class FootstepEditor:
             return None, footstep_err
 
         # open metadata for the event
-        metadata_file_path = Path(event.trial_npz_uri).parent / "metadata.csv"
-        metadata_df = load_metadata(metadata_file_path)
+        try:
+            metadata_file_path = Path(event.trial_npz_uri).parent / "metadata.csv"
+            metadata_df = load_metadata(metadata_file_path)
+        except Exception as e:
+            current_app.logger.error(f"Error loading metadata: {e}")
+            return None, f"Error loading metadata: {e}"
+
+        # open p100 for the event
+        p100, p100_err = self.common._load_npz_from_uri(event.trial_p100_npz_uri)
+        if p100_err or p100 is None:
+            current_app.logger.error(f"Error loading p100 data: {p100_err}")
+            return None, p100_err
 
         # make edits to footstep in df
         footstep_row = metadata_df[metadata_df["FootstepID"] == footstep_id]
@@ -51,20 +61,27 @@ class FootstepEditor:
         ]
 
         # validate footstep bounding box data
-        edited_row = metadata_df[metadata_df["FootstepID"] == footstep_id].iloc[0]
-        if not _is_within_expected_bb_size(edited_row) or _is_within_expect_duration(
-            edited_row
-        ):
-            metadata_df.loc[metadata_df["FootstepID"] == footstep_id, "valid"] = False
-
         metadata_df["valid"] = metadata_df["valid"].apply(bool)
+        edited_row = metadata_df[metadata_df["FootstepID"] == footstep_id].iloc[0]
+        if not _is_within_expected_bb_size(
+            edited_row
+        ) or not _is_within_expect_duration(edited_row):
+            metadata_df.loc[metadata_df["FootstepID"] == footstep_id, "valid"] = False
+        else:
+            metadata_df.loc[metadata_df["FootstepID"] == footstep_id, "valid"] = True
 
         # find path order (anchor identification and path order identification)
+        print("Finding path order...")
         metadata_df["path_order"] = -1  # Any footsteps not on path will be -1
-        identify_anchor_footstep(metadata_df)
-        current_app.logger.info(
-            f"Anchor footstep for event {event_id}\n{metadata_df['path_order']}"
+
+        # trace path of travel and assign path order to footsteps on the path
+        metadata_df["heading_angle"] = metadata_df.apply(
+            get_heading, args=(p100,), axis=1
         )
+
+        metadata_df["path_order"] = metadata_df.apply(reset_path_order, axis=1)
+
+        trace_path(metadata_df)
 
         # take new bounding box data from trial recording and update steps.raw.npz and steps.npz
         # replace metadata.csv for the event to with updated df
@@ -72,20 +89,3 @@ class FootstepEditor:
         # update metrics for event
         # return success or failure
         return
-
-
-def _is_within_expected_bb_size(row):
-    x_length = row.XMax - row.XMin
-    y_length = row.YMax - row.YMin
-
-    short_length = min(x_length, y_length)
-    long_length = max(x_length, y_length)
-
-    return (SHORT_DIMENSTION_MIN < short_length < SHORT_DIMENSTION_MAX) and (
-        LONG_DIMENSION_MIN < long_length < LONG_DIMENSION_MAX
-    )
-
-
-def _is_within_expect_duration(row):
-    duration = row.EndFrame - row.StartFrame
-    return T_MIN < duration < T_MAX
--- backend/storage_access_layer/pipeline/utils/pipeline_utils.py
+++ backend/storage_access_layer/pipeline/utils/pipeline_utils.py
@@ -10,16 +10,15 @@ import pandas as pd
 import numpy as np
 
 import dateutil
+from sklearn.decomposition import PCA
 
+T_MIN = 50
+T_MAX = 400
 
-cc_root = pathlib.Path("data")
-tile_boundaries = [(i * 120, (i + 1) * 120) for i in range(6)]
-
-metadata_configs = []
-_vals = "20 25 30 35 40".split()
-for x in _vals:
-    for y in _vals:
-        metadata_configs += [f"{x}{y}"]
+SHORT_DIMENSTION_MIN = 15
+SHORT_DIMENSTION_MAX = 80
+LONG_DIMENSION_MIN = 40
+LONG_DIMENSION_MAX = 100
 
 
 def parse_identifying_components_from_path(data_filename):
@@ -31,17 +30,6 @@ def parse_identifying_components_from_path(data_filename):
     participant = int(f.parent.parent.parent.parent.name)
 
     return participant, date, direction, swipe_n
-
-
-def load_all_metadata():
-    metadata_filepaths = cc_root.glob("**/metadata.csv")
-
-    dfs = []
-    for f in metadata_filepaths:
-        df = pd.read_csv(f)
-        dfs.append(df)
-
-    return pd.concat(dfs)
 
 
 def load_metadata(filepath):
@@ -87,11 +75,197 @@ def identify_anchor_footstep(metadata):
     min_idx = loss.argmin()
 
     metadata.loc[min_idx, "path_order"] = 0
+    return
 
-    if True:
-        print("Anchor Identification:")
-        for i, (f, l) in enumerate(zip(metadata.FootstepID, loss)):
-            if i == min_idx:
-                print(f"{f}: {l:.02f} *")
-            else:
-                print(f"{f}: {l:.02f}")
+
+def get_heading(row, trial_p100: np.ndarray):
+    bb = row["XMin XMax YMin YMax".split()]
+    p100 = trial_p100[bb.YMin : bb.YMax, bb.XMin : bb.XMax]
+
+    c, r = np.where(p100 > 0)
+    X = np.array([c, r]).T
+
+    pca = PCA(n_components=2)
+    reducer = pca.fit(X)
+    angle_needed_to_upright = np.arctan2(pca.components_[0, 1], pca.components_[0, 0])
+
+    heading_angle = angle_needed_to_upright + 0.5 * np.pi
+
+    tau = 2 * np.pi
+    if heading_angle > tau:
+        heading_angle -= tau
+
+    if heading_angle < 0:
+        heading_angle += tau
+
+    return heading_angle
+
+
+def _is_within_expected_bb_size(row):
+    x_length = row.XMax - row.XMin
+    y_length = row.YMax - row.YMin
+
+    short_length = min(x_length, y_length)
+    long_length = max(x_length, y_length)
+
+    return (SHORT_DIMENSTION_MIN < short_length < SHORT_DIMENSTION_MAX) and (
+        LONG_DIMENSION_MIN < long_length < LONG_DIMENSION_MAX
+    )
+
+
+def _is_within_expect_duration(row):
+    duration = row.EndFrame - row.StartFrame
+    return T_MIN < duration < T_MAX
+
+
+def _get_angle_between(f1, f2):
+    dx = f2.x - f1.x
+    dy = f2.y - f1.y
+    a = np.arctan2(-dy, dx)
+
+    if a < 0:
+        a += 2 * np.pi
+
+    return a
+
+
+## assumes that path order is populated for all steps currently on the path
+def _find_next_footstep(metadata, current_footstep_id):
+    current_footstep = metadata.iloc[current_footstep_id]
+    remaining_candidates = metadata.query("path_order < 0")
+
+    if current_footstep.Direction == "in":
+        # We are tracing backward through time. Next step must occur in some sane window of time before the current one
+        remaining_candidates = remaining_candidates.query(
+            "@current_footstep.t-150 < t < @current_footstep.t-10"
+        )
+    else:
+        # We are tracing forward through time. Next step must occur in some sane window of time after the current one
+        remaining_candidates = remaining_candidates.query(
+            "@current_footstep.t+150 > t > @current_footstep.t+10"
+        )
+
+    ## TODO: Something funny is going on here...
+    distance_to_candidates = np.sqrt(
+        (remaining_candidates.x - current_footstep.x) ** 2
+        + (remaining_candidates.y - current_footstep.y) ** 2
+    )
+
+    remaining_candidates = remaining_candidates[distance_to_candidates < 250]
+
+    if len(remaining_candidates) == 0:
+        return {}
+
+    def normalize_range(a):
+        # This allows is to still pass the stop criteria threshold and be added to the path
+        if len(a) == 1:
+            return np.array([0.1])
+
+        return (a - np.min(a)) / (np.max(a) - np.min(a))
+
+    dys = np.abs(remaining_candidates.y.to_numpy() - current_footstep.y)
+    dxs = np.abs(remaining_candidates.x.to_numpy() - current_footstep.x)
+
+    d_loss = np.sqrt(dxs**2 + dys**2)
+    d_loss = normalize_range(d_loss)
+
+    t_loss = np.abs(remaining_candidates.t.to_numpy() - current_footstep.t)
+    t_loss = normalize_range(t_loss)
+
+    """
+  # in addition to favoring more temporally proximate candidates,
+  # candidates exceedinging a certain absoulte duration threshold should be heavily penalized
+  delta_t = np.abs(remaining_candidates.t.to_numpy() - current_footstep.t)
+  t_absolute_loss = (delta_t / 1000).clip(max=1)
+  """
+
+    angle_btwn = np.array(
+        [
+            _get_angle_between(current_footstep, candidate)
+            for _, candidate in remaining_candidates.iterrows()
+        ]
+    )
+
+    desired_angle = (current_footstep.heading_angle + np.pi) % (2 * np.pi)
+
+    a_loss = np.abs(angle_btwn - desired_angle)
+    a_loss = normalize_range(a_loss)
+
+    loss = (np.sqrt(d_loss) + np.sqrt(t_loss) + np.sqrt(a_loss)) / 3
+
+    loss_dict = {f: l for f, l in zip(remaining_candidates.FootstepID, loss)}
+    return loss_dict
+
+
+def trace_path(metadata):
+    # Start from the anchor footstep
+    current_footstep_id = metadata.query("path_order == 0").FootstepID.iloc[0]
+
+    next_path_order = 1
+    while next_path_order < len(metadata):
+        loss_dict = _find_next_footstep(metadata, current_footstep_id)
+
+        min_loss = 1
+        min_footstep_id = -1
+        for f, l in loss_dict.items():
+            if l <= min_loss:
+                min_loss = l
+                min_footstep_id = f
+
+        next_footstep_id, next_loss = min_footstep_id, min_loss
+
+        # for k, v in loss_dict.items():
+        #     if k == next_footstep_id:
+        #         pass
+        #         # print(f"\t{k}: {v:.2f} *")
+        #     else:
+        #         pass
+        #         # print(f"\t{k}: {v:.2f}")
+
+        # Path termination. Evidence suggesting that we have reached the end of the path:
+        # a) current footstep is near edge of grid
+        # b) current headings points off of grid.
+        # c) all next-step candidates are improbable
+        current_footstep = metadata.iloc[current_footstep_id]
+
+        x = current_footstep.x
+        y = current_footstep.y
+
+        px_from_x_edge = min(x, 480 - x)
+        x_loss = px_from_x_edge / 480
+
+        px_from_y_edge = min(y, 720 - y)
+        y_loss = px_from_y_edge / 720
+
+        edge_loss = min(x_loss, y_loss)
+
+        trajectory_angle = (current_footstep.heading_angle + np.pi) % (2 * np.pi)
+        angle_to_grid_center = np.arctan2(-(360 - y), (240 - x))
+        angle_difference = (trajectory_angle - angle_to_grid_center) % (2 * np.pi)
+        angle_deviation = min(angle_difference, 2 * np.pi - angle_difference)
+        a_loss = 1 - (angle_deviation / np.pi)
+
+        improbable_loss = 1 - next_loss
+
+        # print([f"{l:.2f}" for l in [x_loss, y_loss, a_loss, improbable_loss]])
+
+        stop_loss = (
+            np.sqrt(edge_loss) + np.sqrt(a_loss) + np.sqrt(improbable_loss)
+        ) / 3
+        # print(f"\tstop loss: {stop_loss:.2f}")
+
+        if (len(loss_dict) == 0) or stop_loss < 0.2:
+            break
+
+        metadata.loc[next_footstep_id, "path_order"] = next_path_order
+
+        current_footstep_id = next_footstep_id
+        next_path_order += 1
+
+
+# reset all path nodes EXCEPT for the
+def reset_path_order(row):
+    if row.path_order == 0:
+        return 0
+    else:
+        return -1
